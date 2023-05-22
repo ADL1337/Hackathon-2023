2239a16b5b241a8bbc1cb1f979509c01f4e98b27
SupPepper <lorenzo.lepoivre@gmail.com>
2023-05-22 17:31:24 +0200

    Ajout classe Boss, dialogue

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame, sys
-
+from game_data import WIDTH, HEIGHT
 pygame.init()
 
-screen = pygame.display.set_mode((1000, 800))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
-from boss import Boss1
+
+from boss import *
 single = pygame.sprite.GroupSingle()
-boss = Boss1((0,0), 5, (100, 300), "img/boss1/jump/spr_pepperman_jump_0.png",single)
+boss = Boss1((0,0), 5, (100, 300), "img/boss1/ilde/spr_pepperman_jump_0.png",single)
 
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 50)
