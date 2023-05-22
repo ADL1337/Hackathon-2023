928aea5bc70ccf2856977f346476d2553d35a104
SupPepper <lorenzo.lepoivre@gmail.com>
2023-05-22 15:34:51 +0200

    Ajout classe movableEntity et Projectile

--- entity.py
+++ entity.py
@@ -5,6 +5,17 @@ class Entity(pygame.sprite.Sprite):
         super().__init__(*groups)
         self.image = pygame.image.load(img_path).convert()
         self.rect = self.image.get_rect(topleft=pos)
-        self.mask = self.image.get_masks()
+        self.hitbox = pygame.mask.from_surface(self.image)
 
+class MovableEntity(Entity):
+    def __init__(self, vect_direct, speed, pos, img_path, *groups):
+        super().__init__(pos, img_path, *groups)
+        self.vect = pygame.math.Vector2(vect_direct)
+        self.speed = speed
+
+    def move(self):
+        if self.vect.magnitude() != 0:
+            self.vect = self.vect.normalize()
+        self.rect.x += self.vect[0] * self.speed
+        self.rect.y += self.vect[1] * self.speed
 
