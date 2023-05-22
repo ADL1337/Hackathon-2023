d131469cb83135c331edc97599b69bbd851386ce
D14rn <49399494+D14rn@users.noreply.github.com>
2023-05-22 17:30:36 +0200

    Create Game class and update GameConfig

---
+++ game.py
@@ -0,0 +1,33 @@
+import pygame
+
+from game_data import *
+from utils import GameConfig
+#from menu import Menu # TODO: Create menu.py
+#from game_loop import GameLoop # TODO: Create game_loop.py
+
+
+class Game:
+    pygame.init()
+    pygame.display.set_caption(TITLE)
+    pygame.mixer.init()
+
+    config = GameConfig(CONFIG_PATH)
+    pygame.mixer.music.set_volume(config["VOLUME"]["music"])
+
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+
+    #menu = Menu(screen)
+    #game_loop = GameLoop(screen)
+
+    @classmethod
+    def run(cls):
+        #while True:
+        #    cls.menu.run()
+        #    cls.game_loop.run()
+        pass
+
+
+if __name__ == "__main__":
+    print(Game.config["VOLUME"])
+    print(Game.config["HOTKEYS"])
