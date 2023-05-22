edb079a4434b49f95274dcba4479b80ea9b8efec
SupPepper <lorenzo.lepoivre@gmail.com>
2023-05-22 15:34:51 +0200

    Ajout classe movableEntity et Projectile

--- projectile.py
+++ projectile.py
@@ -1,9 +1,16 @@
 import pygame
-from movableEntity import MovableEntity
+from entity import MovableEntity
 
 class Projectile(MovableEntity):
     def __init__(self, dammage, vect_direct, speed, pos, img_path, *groups):
         super().__init__(vect_direct, speed, pos, img_path, *groups)
         self.dammage = dammage
 
-    
+    def check_dead(self):
+        if not -self.image.get_width()/2 <= self.rect.x <= WIDTH or not -self.image.get_height()/2 <= self.rect.y <= HEIGHT:
+            self.kill()
+
+    def update(self):
+        self.move()
+        self.check_dead()
+
