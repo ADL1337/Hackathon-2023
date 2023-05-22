1796a307b54da4707ccaaafc529779c0726c440d
SupPepper <lorenzo.lepoivre@gmail.com>
2023-05-22 22:56:42 +0200

    Boss 1 quasiement finis

--- boss.py
+++ boss.py
@@ -7,21 +7,27 @@ class Boss1(MovableEntity):
     def __init__(self, vect_direct, speed, pos, img_path, *groups):
         super().__init__(vect_direct, speed, pos, img_path, *groups)
         self.name = "Mafia Boss"
-        self.mode = "Idle"
+        self.mode = "Hurt"
         self.live = 1000
         self.last_live = self.live
-        self.image = pygame.transform.flip(self.image, False, True)
+        self.image = pygame.transform.flip(self.image, True, False)
         self.last_ProjectileCreate = 0
         self.projectile_Timer = 2000
-        self.images_Run = self.create_image_list("img/boss1/fuite/spr_pepperman_rolling_", 12)
-        self.images_Jump = self.create_image_list("img/boss1/jump/spr_pepperman_jump_", 9)
-        self.images_Hurt = self.create_image_list("img/boss1/hurt/spr_pepperman_scared_", 5)
-        self.images_Idle = self.create_image_list(img_path, 12)
         self.time_hurting = 0
         self.pos_possibility = [0, 300, 600]
         self.time_last_dep = 0
         self.blinking_value = [(0,0,0), (128,128,128)]
         self.blinking = False
+        self.graph = None
+
+        self.images_Run = self.create_image_list("img/boss1/fuite/spr_pepperman_rolling_", 12)
+        self.images_Jump = self.create_image_list("img/boss1/jump/spr_pepperman_jump_", 9)
+        self.images_Hurt = self.create_image_list("img/boss1/hurt/spr_pepperman_scared_", 5)
+        self.images_Fall = self.create_image_list("img/boss1/fall/spr_pepperman_groundpoundstart_", 6)
+        self.images_Dead = self.create_image_list("img/boss1/dead/spr_pepperman_hurtplayer_", 3)
+        self.images_Idle = self.create_image_list(img_path, 12)
+        self.images_Make_Grafity = self.create_image_list("img/boss1/make_grafiti/spr_peppermanvengeful_", 3)
+        self.images_Grafity = self.create_image_list("img/boss1/grafiti/grafiti_", 5)
 
     def get_hurt(self):
         self.vect = pygame.math.Vector2(0, 0)
@@ -36,8 +42,9 @@ class Boss1(MovableEntity):
             self.animation(self.images_Run, False)
             if self.rect.x > WIDTH:
                 self.vect = pygame.math.Vector2(0, 0)
-                if pygame.time.get_ticks() - self.time_hurting > 7000:
-                    self.mode = "Coming"
+                self.mode = "Graph"
+                self.graph = Grafity(self.images_Grafity[randint(0, len(self.images_Grafity)-1)])
+                self.state = 0
 
     def get_dammage(self, player_boost):
         if self.mode == "Idle":
@@ -45,15 +52,17 @@ class Boss1(MovableEntity):
             self.image.fill(self.blinking_value[self.blinking], special_flags=pygame.BLEND_RGB_ADD)
             self.blinking = not self.blinking
         if self.live <= 0:
-            self.dead_animation()
+            self.mode = "Dead"
         if self.last_live - self.live > 500:
             self.get_hurt()
 
-    def dead_animation(self):
-        pass
-
     def graphe_animation(self):
-        pass
+        if 6500 <= pygame.time.get_ticks() - self.time_hurting < 12000:
+            self.rect.x, self.rect.y = WIDTH, self.pos_possibility[randint(0, len(self.pos_possibility)-1)]
+            self.graph.update()
+        if pygame.time.get_ticks() - self.time_hurting >= 12000:
+            self.graph.kill()
+            self.mode = "Coming"
 
     def change_pos(self):
         if pygame.time.get_ticks() - self.time_last_dep >= 3000:
@@ -84,7 +93,10 @@ class Boss1(MovableEntity):
                 self.return_to_idle()
 
         elif self.mode == "Jump":
-            self.animation(self.images_Jump, True)
+            if self.vect[1] == -1:
+                self.animation(self.images_Jump, True)
+            else:
+                self.animation(self.images_Fall, True)
             if self.new_pos - self.rect.y == 0:
                 self.return_to_idle()
 
@@ -92,5 +104,20 @@ class Boss1(MovableEntity):
             self.animation(self.images_Idle, True)
             self.change_pos()
 
+        elif self.mode == "Dead":
+            self.animation(self.images_Dead, True)
 
+        elif self.mode == "Graph":
+            self.graphe_animation()
+            if pygame.time.get_ticks() - self.time_hurting < 6500:
+                self.rect = self.image.get_rect(topleft=(0, 0))
+                self.animation(self.images_Make_Grafity, False)
 
+class Grafity(pygame.sprite.Sprite):
+    def __init__(self, skin):
+        super().__init__()
+        self.image = pygame.image.load(skin).convert_alpha()
+        self.rect = self.image.get_rect(topleft=(0,0))
+
+    def update(self):
+        pygame.display.get_surface().blit(self.image, self.rect)
