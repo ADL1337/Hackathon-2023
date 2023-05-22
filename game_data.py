b5049352e1b5d654899cb424553040bfef0ce1f6
D14rn <49399494+D14rn@users.noreply.github.com>
2023-05-22 16:49:03 +0200

    Add game_data file

--- game_data.py
+++ game_data.py
@@ -1,4 +1,12 @@
+from utils import load_config
+
 WIDTH = 1280
 HEIGHT = 780
 FPS = 60
 TITLE = "Pizza Vigilante"
+
+CONFIG_PATH = "res/config.json"
+
+config = load_config(CONFIG_PATH)
+
+VOLUME = config.get("volume")
