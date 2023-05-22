93929bb3bcfe8fdfee8ae228fc2e79a39c346b6a
SupPepper <lorenzo.lepoivre@gmail.com>
2023-05-22 21:18:25 +0200

    Optimisation class Entity et enfants, Amélioration classe Boss1

--- projectile.py
+++ projectile.py
@@ -3,9 +3,9 @@ from game_data import WIDTH, HEIGHT
 from entity import MovableEntity
 
 class Projectile(MovableEntity):
-    def __init__(self, dammage, vect_direct, speed, pos, img_path, *groups):
+    def __init__(self, vect_direct, speed, pos, img_path, limit, *groups):
         super().__init__(vect_direct, speed, pos, img_path, *groups)
-        self.dammage = dammage
+        self.images_for_animation = self.create_image_list(img_path, limit)
 
     def check_dead(self):
         if not -self.image.get_width()/2 <= self.rect.x <= WIDTH + 200 or not -self.image.get_height()/2 <= self.rect.y <= HEIGHT:
