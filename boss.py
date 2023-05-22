0d126543202a97ffd2a883d608c4f512451fd95f
SupPepper <lorenzo.lepoivre@gmail.com>
2023-05-22 21:18:25 +0200

    Optimisation class Entity et enfants, Amélioration classe Boss1

--- boss.py
+++ boss.py
@@ -1,107 +1,96 @@
 import pygame
+from random import randint
 from game_data import WIDTH, HEIGHT
 from entity import MovableEntity
-from projectile import Projectile
 
-class Boss(MovableEntity):
+class Boss1(MovableEntity):
     def __init__(self, vect_direct, speed, pos, img_path, *groups):
-        super().__init__(vect_direct, speed, pos, img_path, *groups)
-        self.state = 0
-
-    def create_image_liste(self, patch, limit):
-        listes = []
-        for i in range(0, limit):
-            listes.append(patch+str(i)+".png")
-        return listes
-
-class Boss1(Boss):
-    def __init__(self, proj_img_path, vect_direct, speed, pos, img_path, *groups):
         super().__init__(vect_direct, speed, pos, img_path, *groups)
         self.name = "Mafia Boss"
         self.mode = "Idle"
-        self.proj_img_path = proj_img_path
+        self.live = 1000
+        self.last_live = self.live
+        self.image = pygame.transform.flip(self.image, False, True)
         self.last_ProjectileCreate = 0
         self.projectile_Timer = 2000
-        self.projectile_Group = pygame.sprite.Group()
-        self.images_Run = self.create_image_liste("img/boss1/fuite/spr_pepperman_rolling_", 12)
-        self.images_Jump = self.create_image_liste("img/boss1/jump/spr_pepperman_jump_", 9)
-        self.images_Hurt = self.create_image_liste("img/boss1/hurt/spr_pepperman_scared_", 5)
-        self.images_Idle = self.create_image_liste("img/boss1/idle/spr_pepperman_idle_", 12)
+        self.images_Run = self.create_image_list("img/boss1/fuite/spr_pepperman_rolling_", 12)
+        self.images_Jump = self.create_image_list("img/boss1/jump/spr_pepperman_jump_", 9)
+        self.images_Hurt = self.create_image_list("img/boss1/hurt/spr_pepperman_scared_", 5)
+        self.images_Idle = self.create_image_list(img_path, 12)
+        self.time_hurting = 0
+        self.pos_possibility = [0, 300, 600]
+        self.time_last_dep = 0
+        self.blinking_value = [(0,0,0), (128,128,128)]
+        self.blinking = False
 
-    def create_projectile(self):
-        proj = Projectile(1, (-1, 0), 5, (WIDTH + 100), self.proj_img_path, self.projectile_Group)
+    def get_hurt(self):
+        self.vect = pygame.math.Vector2(0, 0)
+        self.time_hurting = pygame.time.get_ticks()
+        self.mode = "Hurt"
 
-    def idle_animation(self):
-        self.image = pygame.image.load(self.images_Idle[self.state]).convert()
-        self.state += 1
-        if self.state > 11:
-            self.state = 0
+    def hurt_animation(self):
+        if not pygame.time.get_ticks() - self.time_hurting > 2000:
+            self.animation(self.images_Hurt, True)
+        else:
+            self.vect = pygame.math.Vector2(1, 0)
+            self.animation(self.images_Run, False)
+            if self.rect.x > WIDTH:
+                self.vect = pygame.math.Vector2(0, 0)
+                if pygame.time.get_ticks() - self.time_hurting > 7000:
+                    self.mode = "Coming"
 
-    def fuite_animation(self):
-        self.move()
-        self.image = pygame.image.load(self.images_Run[self.state]).convert()
-        self.state += 1
-        if self.state > 11:
-            self.state = 0
+    def get_dammage(self, player_boost):
+        if self.mode == "Idle":
+            self.live -= player_boost * 1
+            self.image.fill(self.blinking_value[self.blinking], special_flags=pygame.BLEND_RGB_ADD)
+            self.blinking = not self.blinking
+        if self.live <= 0:
+            self.dead_animation()
+        if self.last_live - self.live > 500:
+            self.get_hurt()
 
-    def jump_animation(self):
-        self.move()
-        self.image = pygame.image.load(self.images_Jump[self.state]).convert()
-        self.state += 1
-        if self.state > 8:
-            self.state = 0
+    def dead_animation(self):
+        pass
 
-    def take_dammage_animation(self):
-        self.image = pygame.image.load(self.images_Hurt[self.state]).convert()
-        self.state += 1
-        if self.state > 4:
-            self.state = 0
+    def graphe_animation(self):
+        pass
 
-    def update(self):
-        if self.mode == "Hurt":
-            self.vect = (0, 0)
-            self.take_dammage_animation()
-        elif self.mode == "Fuite":
-            self.vect = (1, 0)
-            self.fuite_animation()
-        elif self.mode == "Jump":
-            self.vect = (0, -1)
-            self.jump_animation()
-        elif self.mode == "Idle":
-            self.vect = (0, 0)
-            self.idle_animation()
+    def change_pos(self):
+        if pygame.time.get_ticks() - self.time_last_dep >= 3000:
+            self.new_pos = self.pos_possibility[randint(0, len(self.pos_possibility)-1)]
+            if self.new_pos != self.rect.y:
+                self.vect = pygame.math.Vector2(0, 1)
+                if self.new_pos < self.rect.y:
+                    self.vect = pygame.math.Vector2(0, -1)
+                self.mode = "Jump"
+                self.state = 0
 
-class Boss2(Boss):
-    def __init__(self, proj_img_path, vect_direct, speed, pos, img_path, *groups):
-        super().__init__(vect_direct, speed, pos, img_path, *groups)
-        self.name = "Tchad Pillar"
+    def return_to_idle(self):
+        self.state = 0
+        self.vect = pygame.math.Vector2(0, 0)
+        self.time_last_dep = pygame.time.get_ticks()
         self.mode = "Idle"
-        self.proj_img_path = proj_img_path
-        self.last_ProjectileCreate = 0
-        self.projectile_Timer = 2000
-        self.projectile_Group = pygame.sprite.Group()
-        self.images_Hurt = self.create_image_liste("img/boss2/hurt/)  #DEVOIR SEPARER LES TILES SHEET AVEC UNE CLASSE
-        self.images_Idle = self.create_image_liste("img/boss2/idle/)
-
-    def create_projectile(self):
-        proj = Projectile(1, (-1, 0), 5, (WIDTH + 100), self.proj_img_path, self.projectile_Group)
-
-    def idle_animation(self):
-        self.image = pygame.image.load(self.images_Idle[self.state]).convert()
-        self.state += 1
-        if self.state > 11:
-            self.state = 0
-
-    def take_dammage_animation(self):
-        self.image = pygame.image.load(self.images_Hurt[self.state]).convert()
-        self.state += 1
-        if self.state > 4:
-            self.state = 0
 
     def update(self):
+        self.move()
+
         if self.mode == "Hurt":
-            self.vect = (0, 0)
-            self.take_dammage_animation()
+            self.hurt_animation()
+
+        elif self.mode == "Coming":
+            self.vect = pygame.math.Vector2(-1, 0)
+            self.animation(self.images_Run, True)
+            if self.rect.x == 1000:
+                self.return_to_idle()
+
+        elif self.mode == "Jump":
+            self.animation(self.images_Jump, True)
+            if self.new_pos - self.rect.y == 0:
+                self.return_to_idle()
+
         elif self.mode == "Idle":
-            self.vect = (0, 0)
-            self.idle_animation()
+            self.animation(self.images_Idle, True)
+            self.change_pos()
+
+
+
--- entity.py
+++ entity.py
@@ -1,9 +1,11 @@
 import pygame
 
+# ! Format d'image requis nom_numéro.png (Toujours compté à partir de 0 mettre numéro à 0 même si image seul)
+# Exemple boulette_0.png
 class Entity(pygame.sprite.Sprite):
-    def __init__(self, pos, img_path, *groups):
+    def __init__(self, pos, img_path, *groups): #img_path = chemin de l'image sans le 'numéro.png'
         super().__init__(*groups)
-        self.image = pygame.image.load(img_path).convert()
+        self.image = pygame.image.load(img_path+"0.png").convert_alpha() #Prend au départ la première ou la seule image de la collection
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = pygame.mask.from_surface(self.image)
 
@@ -12,10 +14,26 @@ class MovableEntity(Entity):
         super().__init__(pos, img_path, *groups)
         self.vect = pygame.math.Vector2(vect_direct)
         self.speed = speed
+        self.state = 0
 
     def move(self):
         if self.vect.magnitude() != 0:
             self.vect = self.vect.normalize()
         self.rect.x += self.vect[0] * self.speed
         self.rect.y += self.vect[1] * self.speed
+
+    def animation(self, liste, need_mirror):
+        if need_mirror:
+            self.image = pygame.transform.flip(pygame.image.load(liste[self.state]).convert_alpha(), True, False)
+        else:
+            self.image = pygame.image.load(liste[self.state]).convert_alpha()
+        self.state += 1
+        if self.state > len(liste) - 1:
+            self.state = 0
+
+    def create_image_list(self, path, limit):
+        listes = []
+        for i in range(0, limit):
+            listes.append(path+str(i)+".png")
+        return listes
 
