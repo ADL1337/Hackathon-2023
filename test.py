59d2fcf119701a31edac1b08dd4bc917bdb8949c
ADL1337 <arnaud.2.lena@gmail.com>
2023-05-23 14:38:16 +0200

    ajout de tests

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
+from entity import Entity
 from boss import *
 single = pygame.sprite.GroupSingle()
 boss = Boss1((0,0), 5, (1000, 300), "img/boss1/idle/spr_pepperman_idle_")
@@ -16,6 +17,12 @@ SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 50)
 player = Player((0,0), 10, (1000, 300), "img/player/idle/player_idle_")
 level = Level(boss, screen, player)
+test_wall = Entity((100,300), "img/projectiles/meatball/meatball_")
+test_walls = [Entity((0 + i * 100, 600), "img/projectiles/meatball/meatball_") for i in range(13)]
+level.obstacle_sprites.add(test_wall)
+level.obstacle_sprites.add(test_walls)
+level.visibles_sprites.add(test_wall)
+level.visibles_sprites.add(test_walls)
 
 while True:
     for event in pygame.event.get():
