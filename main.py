7b920a332d5acf6b8f1ce7a59b2b341dc878ab8d
Evoks01 <130904295+Evoks01@users.noreply.github.com>
2023-05-23 15:56:55 +0200

    dialog box V2

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ ecran = pygame.display.set_mode((largeur_ecran, hauteur_ecran))
 BLANC = (255, 255, 255)
 NOIR = (0, 0, 0)
 
- # Polices de caractères
+# Polices de caractères
 police = pygame.font.SysFont("Arial", 24)
 
 # Boîte de dialogue
@@ -26,7 +26,7 @@ dialogue = [
 # Variables de défilement du dialogue
 indice_dialogue = 0
 temps_dialogue = 0
-delai_dialogue = 1500  # Délai entre chaque message en millisecondes
+delai_dialogue = 2000  # Délai entre chaque message en millisecondes
 
 # Boucle principale du jeu
 en_cours = True
@@ -35,17 +35,24 @@ while en_cours:
     for evenement in pygame.event.get():
         if evenement.type == pygame.QUIT:
             en_cours = False
+        elif evenement.type == pygame.MOUSEBUTTONDOWN:
+            # Passage au dialogue suivant lors d'un clic de souris
+            indice_dialogue += 1
+            temps_dialogue = pygame.time.get_ticks()
 
     ecran.fill(BLANC)
 
     # Affichage du dialogue
     if indice_dialogue < len(dialogue):
         message = police.render(dialogue[indice_dialogue], True, NOIR)
-        ecran.blit(message, (50, hauteur_ecran // 10))
-        temps_actuel = pygame.time.get_ticks()
-        if temps_actuel - temps_dialogue >= delai_dialogue:
-            temps_dialogue = temps_actuel
-            indice_dialogue += 1
+        rect_message = message.get_rect(center=(largeur_ecran // 2, hauteur_ecran // 2))
+        ecran.blit(message, rect_message)
+
+    # Passage automatique au dialogue suivant après un délai
+    temps_actuel = pygame.time.get_ticks()
+    if temps_actuel - temps_dialogue >= delai_dialogue:
+        indice_dialogue += 1
+        temps_dialogue = temps_actuel
 
     pygame.display.flip()
     horloge.tick(60)
