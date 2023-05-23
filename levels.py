e9a7b362dda81953c06330f5a5fa85dccda571bc
Akhy <101968038+Akhyy@users.noreply.github.com>
2023-05-23 13:47:33 +0200

    changement complet de la méthode + tout en 1 seul fichier

--- levels.py
+++ levels.py
@@ -1,26 +1,116 @@
 import pygame
-from layoutimport import import_csv_layout
-from leveltest import tile
-from tile import Tile
+import csv
 
-class Level:
-    def __init__(self, level_data, surface):
-        self.display_surface = surface
-        terrain_layout = import_csv_layout(level_data['terrain'])
-        self.terrain_sprites = self.create_tile_group(terrain_layout, 'terrain')
+pygame.init()
 
-    def create_tile_group(self, layout, type):
-        sprite_group = pygame.sprite.Group()
-        for row_index, row  in enumerate(layout):
-            for col_index, val in enumerate(row):
-                if val != -1:
-                    x = col_index * tile_size
-                    y = row_index * tile_size
-                    if type == 'terrain':
-                        sprite = Tile(tile_size, x,y)
-                        sprite_group.add(sprite)
+largeur_ecran = 1080
+hauteur_ecran = 720
+ecran = pygame.display.set_mode((largeur_ecran, hauteur_ecran))
 
-        return sprite_group
-    
-    def run(self):
-        self.terrain_sprites.draw()
+# Chargement de la carte depuis un fichier CSV
+def charger_carte(nom_fichier):
+    carte = []
+    with open(nom_fichier, newline='') as fichier_csv:
+        lecteur_csv = csv.reader(fichier_csv, delimiter=',')
+        for ligne in lecteur_csv:
+            carte.append(list(map(int, ligne)))
+    return carte
+
+GRAVITE = 0.5
+VITESSE_MAX = 3
+BLANC = (255, 255, 255)
+BLEU = (0, 0, 255)
+
+class Joueur(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((32, 32))
+        self.image.fill(BLEU)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.vx = 0
+        self.vy = 0
+        self.saut = False
+
+    def update(self):
+        self.gravite()
+
+        self.rect.x += self.vx
+        blocs_collisionnes = pygame.sprite.spritecollide(self, blocs, False)
+        for bloc in blocs_collisionnes:
+            if self.vx > 0:
+                self.rect.right = bloc.rect.left
+            elif self.vx < -1:
+                self.rect.left = bloc.rect.right
+
+        self.rect.y += self.vy
+        blocs_collisionnes = pygame.sprite.spritecollide(self, blocs, False)
+        for bloc in blocs_collisionnes:
+            if self.vy > 0:
+                self.rect.bottom = bloc.rect.top
+                self.saut = False
+            elif self.vy < 0:
+                self.rect.top = bloc.rect.bottom
+
+    def gravite(self):
+        if self.vy == 0:
+            self.vy = 1
+        else:
+            self.vy += GRAVITE
+        if self.vy > VITESSE_MAX:
+            self.vy = VITESSE_MAX
+
+    def sauter(self):
+        if not self.saut:
+            self.vy = -10
+            self.saut = True
+
+class Bloc(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((16, 16))
+        self.image.fill(BLANC)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+joueur = Joueur(16, 16)
+blocs = pygame.sprite.Group()
+carte = charger_carte("map.csv")
+for ligne in range(len(carte)):
+    for colonne in range(len(carte[ligne])):
+        if carte[ligne][colonne] > 1:
+            bloc = Bloc(colonne * 16, ligne * 16)
+            blocs.add(bloc)
+        if carte[ligne][colonne] == 699:
+            print('mort')
+
+running = True
+horloge = pygame.time.Clock()
+while running:
+    for evenement in pygame.event.get():
+        if evenement.type == pygame.QUIT:
+            running = False
+        elif evenement.type == pygame.KEYDOWN:
+            if evenement.key == pygame.K_SPACE:
+                joueur.sauter()
+
+    touches = pygame.key.get_pressed()
+    if touches[pygame.K_LEFT]:
+        joueur.vx = -5
+    elif touches[pygame.K_RIGHT]:
+        joueur.vx = 5
+    else:
+        joueur.vx = 0
+
+    joueur.update()
+
+    ecran.fill((0, 0, 0))
+    blocs.draw(ecran)
+    ecran.blit(joueur.image, joueur.rect)
+
+    pygame.display.flip()
+    horloge.tick(60)
+
+pygame.quit()
