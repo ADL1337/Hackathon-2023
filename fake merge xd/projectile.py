9f2043166d2ea1dee36c14d4769872b1bc62230e
Akhy <101968038+Akhyy@users.noreply.github.com>
2023-05-24 14:41:26 +0200

    clear directories

--- fake merge xd/projectile.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from movableEntity import MovableEntity
-
-class Projectile(MovableEntity):
-    def __init__(self, dammage, vect_direct, speed, pos, img_path, *groups):
-        super().__init__(vect_direct, speed, pos, img_path, *groups)
-        self.dammage = dammage
-
-    
