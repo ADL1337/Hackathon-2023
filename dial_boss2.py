e4308b883e034c58ae60e662ac6bb8b913fef7c9
Evoks01 <130904295+Evoks01@users.noreply.github.com>
2023-05-24 11:44:37 +0200

    Add files via upload

---
+++ dial_boss2.py
@@ -0,0 +1,8 @@
+#dialogue boss 2
+
+perso_Diag1 = ["Où  est peppino spaghetti ?/n Oh, Pizza militaire, tu es si confiant. Mais tu ne le retrouvera jamais.Tes pouvoirs extraterrestres ne me font pas peur. Je suis la meilleure pizza de combat de tout l'univers et je compte bien le retrouver."]
+perso_Diag2 = ["Tes pouvoirs extraterrestres ne me font pas peur. Je suis la meilleure /n pizza de combat de tout l'univers et je compte bien le retrouver. /n Nous allons voir si ta garniture militaire peut résister à ma force cosmique./n Prépare-toi à goûter à la défaite."]
+perso_Diag3 = ["Je suis prête à tout pour le retrouver, /n Tes pouvoir de pizza ne sont rien comparées à mes pouvoir cosmiques./n  Je vais te montrer ce que signifie être hors de ce monde"]
+
+dialoge1 = [perso_Diag1, perso_Diag2, perso_Diag3,]
+print(dialoge1)
