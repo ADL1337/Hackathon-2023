8463c2564f6f9f9efa59996c32ba742769008393
Evoks01 <130904295+Evoks01@users.noreply.github.com>
2023-05-24 11:36:58 +0200

    Add files via upload

---
+++ dialogue_boss1.py
@@ -0,0 +1,14 @@
+# dialogue
+
+#boss1
+
+perso_Diag1 = ["Hé, Piquanto ! Prêt à te faire mettre K.O ? /n Ha ! Tu penses vraiment pouvoir entrer sur mon territoire comme ça ?"]
+perso_Diag2 = ["Je vais te montrer qu’à l’armée on n’est pas des zoulettes ! /n Sache que tu ne fais pas le poids contre moi."]
+perso_Diag3 = [" Ne sois pas si sûr de toi ! je vais te faire mordre la poussière avant /n même que tu ne le réalises. /n Fais de ton mieux. Mais je vais te montrer ce que signifie vraiment /n avoir du cran."]
+perso_Diag4 = ["Les pizzas ne sont pas les seules choses que je sais livrer. Je suis prêt à /n te servir une bonne leçon. /n Tu te surestimes. Tu n'as aucune idée de ce qui t'attend, je ne ferai /n aucune exception pour toi."]
+perso_Diag5 = ["Je me battrai jusqu'à mon dernier souffle. Prépare-toi à sentir la /n transpiration de mes couilles ! /n Tant de paroles, mais seuls les actes comptent. Et crois-moi, je vais te /n montrer ce que signifie être un véritable chef de la mafia."]
+perso_diag6 = [" Je ne reculerai pas, j'ai l'honneur de mon côté et je me battrai avec /n bravoure. Prépare-toi à être vaincu par le meilleur pizzaïolo soldat !"]
+boss_Diag6 = ["On verra bien qui sortira vainqueur de ce combat. /n Nous verrons, que le duel commence ! J’arrive te sauver Peppino Spaghetti ! /n"]
+
+dialoge1 = [perso_Diag1, perso_Diag2, perso_Diag3, perso_Diag4, perso_Diag5, perso_diag6, boss_Diag6]
+print(dialoge1)
